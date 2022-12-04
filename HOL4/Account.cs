076d006f2abf539ed2db7e1d19120caa35bd77af
SunkavalliBhargavi <118039105+SunkavalliBhargavi@users.noreply.github.com>
2022-12-04 06:06:21 +0000

    Add files via upload

---
+++ HOL4/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace HOL4.Models
+{
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Range(1, long.MaxValue, ErrorMessage = "Account number cannot be negative or zero")]
+        [StringLength(byte.MaxValue,MinimumLength =2,ErrorMessage ="Minimum length must be 2")]
+        public int AccountNumber { get; set; }
+        public string Name { get; set; }
+        [Range(500, long.MaxValue, ErrorMessage = "Minimum balance must be 500")]
+        public double CurrentBalance { get; set; }
+    }
+}
---
+++ HOL4/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using HOL4.Models;
+
+
+namespace HOL4.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDbContext context = new MyDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Accounts);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult CreateAccount(Account a)
+        {
+           // if (a.AccountNumber < 0)
+          //  {
+            //    ModelState.AddModelError("AccountNumber", "Account number cannot be negative");
+            //}
+           // if (String.IsNullOrEmpty(a.Name))
+            //{
+              //  ModelState.AddModelError("Name", "Account holder's name is required");
+            //}
+            //if ((a.CurrentBalance >= 1 && a.CurrentBalance < 500) || a.CurrentBalance < 0)
+           // {
+             //   ModelState.AddModelError("CurrentBalance", "Minimum balance must be atleast 500");
+           // }
+            if (ModelState.IsValid)
+            {
+                context.Accounts.Add(a);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+        public ActionResult Edit(int ?accno)
+        {
+            var account_to_edit = (from a in context.Accounts where a.AccountNumber == accno select a).SingleOrDefault();
+            return View(account_to_edit);
+        }
+        public ActionResult EditAccount(Account a)
+        {
+            context.Entry<Account>(a).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int ?accno)
+        {
+            var account_to_delete = (from a in context.Accounts where a.AccountNumber == accno select a).SingleOrDefault();
+            context.Entry<Account>(account_to_delete).State = System.Data.Entity.EntityState.Deleted;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [ChildActionOnly]
+        public ActionResult GetNews(string category)
+        {
+            return PartialView(null, category);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
